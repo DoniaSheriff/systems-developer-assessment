469b712b18a01341b9ebc612b1b53d8102a18b9e
DoniaSheriff <44745727+DoniaSheriff@users.noreply.github.com>
2020-11-28 19:46:02 +0200

    change the global asax and the packages config

--- src/NetC.JuniorDeveloperExam.Web/Global.asax.cs
+++ src/NetC.JuniorDeveloperExam.Web/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System.Web;
+﻿using Microsoft.Extensions.DependencyInjection;
+using NetC.JuniorDeveloperExam.Web.Services;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -6,6 +8,11 @@ namespace NetC.JuniorDeveloperExam.Web
 {
     public class MvcApplication : HttpApplication
     {
+        private readonly IBlogPosts blogPosts;
+        public MvcApplication()
+        {
+            this.blogPosts = new BlogPostsService();
+        }
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
