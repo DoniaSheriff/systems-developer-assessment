fbab4527b032ba0213a755accf7dd2e3dbe32db0
DoniaSheriff <44745727+DoniaSheriff@users.noreply.github.com>
2020-11-28 19:55:16 +0200

    add the change of the app_data , and the app_start

--- src/NetC.JuniorDeveloperExam.Web/App_Start/RouteConfig.cs
+++ src/NetC.JuniorDeveloperExam.Web/App_Start/RouteConfig.cs
@@ -8,6 +8,19 @@ namespace NetC.JuniorDeveloperExam.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+              name: "Default",
+              url: "{controller}/{action}",
+              defaults: new { controller = "BlogPosts", action = "Index" });
+            routes.MapRoute(
+              name: "BlogPosts",
+              url: "{controller}/{action}/{id}",
+              defaults: new { controller = "BlogPosts", action = "GetBlogPosts", id = UrlParameter.Optional });
+            routes.MapRoute(
+              name: "Default2",
+              url: "{controller}/{action}",
+              defaults: new { controller = "BlogPosts", action = "GetBlogPosts", id = UrlParameter.Optional });
+
         }
     }
 }
