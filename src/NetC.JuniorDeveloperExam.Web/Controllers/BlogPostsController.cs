b7a6cd571810681d110e84493560aad3b6cde203
DoniaSheriff <44745727+DoniaSheriff@users.noreply.github.com>
2020-11-28 19:58:01 +0200

    add the scripts

---
+++ src/NetC.JuniorDeveloperExam.Web/Controllers/BlogPostsController.cs
@@ -0,0 +1,38 @@
+﻿using NetC.JuniorDeveloperExam.Web.Models;
+using NetC.JuniorDeveloperExam.Web.Services;
+using System.Web.Mvc;
+
+namespace NetC.JuniorDeveloperExam.Web.Controllers
+{
+    public class BlogPostsController : Controller
+    {
+        private readonly IBlogPosts BlogPostsService;
+        public BlogPostsController()
+        {
+            this.BlogPostsService = new BlogPostsService();
+        }
+
+        public ActionResult Index()
+        {
+
+            ViewBag.Model = BlogPostsService.GetBlogs();
+            return View();
+        }
+        public ActionResult GetBlogPosts(int? id)
+        {
+            var model = BlogPostsService.GetBlog(id);
+            if (model != null) ViewBag.Model = model;
+            else
+                return RedirectToAction("Index");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(CommentModel commentModel)
+        {
+            if (BlogPostsService.AddComment(commentModel))
+                return RedirectToAction("GetBlogPosts", new { id = commentModel.BlogId });
+            else return View();
+        }
+    }
+}
---
+++ src/NetC.JuniorDeveloperExam.Web/Models/BlogsModel.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetC.JuniorDeveloperExam.Web.Models
+{
+    public class Blog
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("image")]
+        public string Image { get; set; }
+
+        [JsonProperty("htmlContent")]
+        public string HtmlContent { get; set; }
+
+        [JsonProperty("comments")]
+        public List<Comment> Comments { get; set; }
+    }
+   public class Comment
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+        [JsonProperty("emailAddress")]
+        public string EmailAddress { get; set; }
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+    public class BlogPostsClass
+    {
+        [JsonProperty("blogPosts")]
+        public List<Blog> BlogPosts { get; set; }
+    }
+}
